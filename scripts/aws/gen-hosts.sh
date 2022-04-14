2930d63078c518c15ee3222e7c284218264ed33c
Midhul <midhul.v@gmail.com>
2022-04-14 10:53:19 +0530

    Update gen-hosts.sh

---
+++ scripts/aws/gen-hosts.sh
@@ -0,0 +1,5 @@
+aws ec2 describe-instances --filters 'Name=tag:name,Values=shortstack-ae' 'Name=tag:type,Values=server' 'Name=instance-state-name,Values=running' --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --output text > $hosts_file
+echo "Generated hosts file"
+
+echo "Client VM (private_ip    public_ip)"
+aws ec2 describe-instances --filters 'Name=tag:name,Values=shortstack-ae' 'Name=tag:type,Values=client' 'Name=instance-state-name,Values=running' --query 'Reservations[*].Instances[*].[PrivateIpAddress,PublicIpAddress]' --output text | head
