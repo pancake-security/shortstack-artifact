5bed8c9673af42b97a0014f0e71aba3d75c29eeb
Midhul <midhul.v@gmail.com>
2022-04-14 22:28:27 -0400

    exp 4

---
+++ exp4/collect_exp4.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+srcdir="`dirname "$0"`"
+srcdir="`cd "$srcdir"; pwd`"
+
+source $srcdir/../envs.sh
+
+sbin=$SHORTSTACK_ROOT/sbin
+export SHORTSTACK_SERVERS=$SHORTSTACK_HOSTS
+
+config=$1
+
+cat $srcdir/data/$config.xput | head -n 1600 | tail -n 200 > $srcdir/data/$config.xputgraph
+
+echo "Wrote graph data to data/$config.xputgraph"
