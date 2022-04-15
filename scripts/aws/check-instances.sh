468cf25881e603ad77ee6d91e630a89c470ad181
Midhul <mvv25@cornell.edu>
2022-04-15 04:34:11 +0000

    minor update

--- scripts/aws/check-instances.sh
+++ scripts/aws/check-instances.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-hosts_file=$1
 
 echo "Number of running instances"
 aws ec2 describe-instances --filters 'Name=tag:name,Values=shortstack-ae' 'Name=instance-state-name,Values=running' --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --output text | wc -l
--- scripts/aws/gen-hosts.sh
+++ scripts/aws/gen-hosts.sh
@@ -1,3 +1,5 @@
+hosts_file=$1
+
 aws ec2 describe-instances --filters 'Name=tag:name,Values=shortstack-ae' 'Name=tag:type,Values=server' 'Name=instance-state-name,Values=running' --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --output text > $hosts_file
 echo "Generated hosts file"
 
