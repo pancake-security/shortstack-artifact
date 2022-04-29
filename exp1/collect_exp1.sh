988c6e26f231f07df581e6d1752f9fd3c534ea6a
Midhul <midhul.v@gmail.com>
2022-04-29 06:48:57 -0400

    Minor update

--- exp1/collect_exp1.sh
+++ exp1/collect_exp1.sh
@@ -10,7 +10,8 @@ export SHORTSTACK_SERVERS=$SHORTSTACK_HOSTS
 
 config=$1
 
-echo "X 	Throughput(ops/sec) 	Latency(microseconds)"
+echo "X 	Throughput(ops/sec) 	Latency-including-WAN(ms)"
+# For estimate of WAN latency we use average latency between AWS us-east-1 and us-west-2 regions measured over 1 week period (73.59ms)
 for x in 1 2 3 4; do
-	paste <(echo $x) <(cat $srcdir/data/$config-x$x.stats | grep -i "xput" | awk '{print $2}') <(cat $srcdir/data/$config-x$x.stats | grep -i "average latency" | awk '{print $3}')
+	paste <(echo $x) <(cat $srcdir/data/$config-x$x.stats | grep -i "xput" | awk '{print $2}') <(cat $srcdir/data/$config-x$x.stats | grep -i "average latency" | awk '{print (73.59+$3)/100;}')
 done
